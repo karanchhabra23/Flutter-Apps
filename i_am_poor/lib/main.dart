113bf1a5c4930b5a254508e6b0c3ed61e970e327
karankc23 <karanrssb23@gmail.com>
2021-07-29 03:25:04 +0530

    29-07-2021 i am poor

---
+++ i_am_poor/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+        home: Scaffold(
+      backgroundColor: Color(0xFFC0F2B7),
+      appBar: AppBar(
+        title: Center(child: Text('I AM POOR')),
+        backgroundColor: Color(0xFF53FAC9),
+      ),
+      body: Center(
+        child: Image(
+          image: AssetImage('Image/poor.jpg'),
+        ),
+      ),
+    )),
+  );
+}
